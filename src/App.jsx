b786061efacacca729da6351d8188fb5c402ae9b
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-29 20:48:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import AppRoutes from './AppRoutes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+
 function App() {
   return (
     <HelmetProvider>
-      <AppRoutes />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow">
+          <AppRoutes />
+        </div>
+        <Footer />
+      </div>
       <Toaster position="top-right" />
     </HelmetProvider>
   );
