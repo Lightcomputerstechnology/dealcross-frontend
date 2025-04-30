2022e2e9646da4fc93ef2125f08104244be66757
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-30 18:45:17 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['react-csv'],
+    exclude: ['react-feather', 'prop-types'],
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    commonjsOptions: {
+      include: [/node_modules/, /react-csv/],
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+  envPrefix: 'VITE_',
+});
