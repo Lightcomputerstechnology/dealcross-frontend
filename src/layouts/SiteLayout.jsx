5cb9ea122982a875c278c7f01dee61c6d0efcd65
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-01 19:06:01 +0100

    Update SiteLayout.jsx

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,25 +1,38 @@
-// File: src/layouts/SiteLayout.jsx import React from 'react'; import { Outlet } from 'react-router-dom'; import SEOHead from '@/components/SEOHead'; import PromoBanner from '@/components/PromoBanner'; import Navbar from '@/components/Navbar'; import Footer from '@/components/Footer'; import BackToTopButton from '@/components/BackToTopButton';
+// File: src/layouts/SiteLayout.jsx
 
-const SiteLayout = () => { return ( <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition duration-300 overflow-x-hidden"> {/* Watermark Background, if desired /} {/ <img src={Logo} alt="Watermark" className="absolute inset-0 w-full h-full object-contain opacity-5 pointer-events-none z-0 select-none" /> */}
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-{/* SEO and Promo Banner */}
-  <SEOHead />
-  <PromoBanner />
+import SEOHead          from '@/components/SEOHead';
+import PromoBanner      from '@/components/PromoBanner';
+import Navbar           from '@/components/Navbar';
+import Footer           from '@/components/Footer';
+import BackToTopButton  from '@/components/BackToTopButton';
+import Logo             from '@/assets/images/dealcross-logo.png';
 
-  {/* Navigation */}
-  <Navbar />
+const SiteLayout = () => (
+  <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white overflow-x-hidden">
+    {/* Watermark Background */}
+    <img
+      src={Logo}
+      alt="Watermark"
+      className="absolute inset-0 w-full h-full object-contain opacity-5 pointer-events-none select-none"
+    />
 
-  {/* Page Content */}
-  <main className="flex-grow">
-    <Outlet />
-  </main>
+    {/* Main Overlay */}
+    <div className="relative z-10 flex flex-col flex-1">
+      <SEOHead />
+      <PromoBanner />
+      <Navbar />
 
-  {/* Footer and Utilities */}
-  <Footer />
-  <BackToTopButton />
-</div>
+      <main className="flex-grow">
+        <Outlet />
+      </main>
 
-); };
+      <Footer />
+      <BackToTopButton />
+    </div>
+  </div>
+);
 
 export default SiteLayout;
-
