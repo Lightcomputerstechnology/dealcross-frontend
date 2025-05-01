f52e2a4fd6ffb451639dabcd1b1711268d16a3f7
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-01 18:58:52 +0100

    Update SiteLayout.jsx

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,43 +1,25 @@
-// File: src/layouts/SiteLayout.jsx
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-+ import SEOHead from '@/components/SEOHead';
-+ import PromoBanner from '@/components/PromoBanner';
-  import Navbar from '@/components/Navbar';
-  import Footer from '@/components/Footer';
-  import BackToTopButton from '@/components/BackToTopButton';
-  import Logo from '@/assets/images/dealcross-logo.png';
+// File: src/layouts/SiteLayout.jsx import React from 'react'; import { Outlet } from 'react-router-dom'; import SEOHead from '@/components/SEOHead'; import PromoBanner from '@/components/PromoBanner'; import Navbar from '@/components/Navbar'; import Footer from '@/components/Footer'; import BackToTopButton from '@/components/BackToTopButton';
 
-const SiteLayout = () => (
-  <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition duration-300 overflow-x-hidden">
-    {/* Watermark Background */}
-    <img
-      src={Logo}
-      alt="Watermark"
-      className="absolute inset-0 w-full h-full object-contain opacity-5 pointer-events-none select-none"
-    />
+const SiteLayout = () => { return ( <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition duration-300 overflow-x-hidden"> {/* Watermark Background, if desired /} {/ <img src={Logo} alt="Watermark" className="absolute inset-0 w-full h-full object-contain opacity-5 pointer-events-none z-0 select-none" /> */}
 
-    {/* Main Overlay */}
-    <div className="relative z-10 flex flex-col min-h-screen">
-+     {/* SEO meta tags */}
-+     <SEOHead />
-+
-+     {/* Site-wide promo banner */}
-+     <PromoBanner />
+{/* SEO and Promo Banner */}
+  <SEOHead />
+  <PromoBanner />
 
-      {/* Navigation */}
-      <Navbar />
+  {/* Navigation */}
+  <Navbar />
 
-      {/* Page content */}
-      <main className="flex-grow">
-        <Outlet />
-      </main>
+  {/* Page Content */}
+  <main className="flex-grow">
+    <Outlet />
+  </main>
 
-      {/* Footer & helpers */}
-      <Footer />
-      <BackToTopButton />
-    </div>
-  </div>
-);
+  {/* Footer and Utilities */}
+  <Footer />
+  <BackToTopButton />
+</div>
+
+); };
 
 export default SiteLayout;
+
