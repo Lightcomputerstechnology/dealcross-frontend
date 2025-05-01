4bea7820cfb51179a20662e7d6b13b9cb10617b4
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-01 18:14:48 +0100

    Update SiteLayout.jsx

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,25 +1,18 @@
-// File: src/layouts/SiteLayout.jsx
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import SEOHead from '@/components/SEOHead';
-import PromoBanner from '@/components/PromoBanner';
-import BackToTopButton from '@/components/BackToTopButton';
+-import AppRoutes from './AppRoutes.jsx';
++import App from './App.jsx';
 
-const SiteLayout = () => (
-  <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900">
-    <SEOHead />
-    <PromoBanner />
-    <Navbar />
-
-    <main className="flex-grow p-4">
-      <Outlet />
-    </main>
-
-    <Footer />
-    <BackToTopButton />
-  </div>
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <NotificationProvider>
+        <UserProvider>
+-         <div className="animate-fade-in">
+-           <AppRoutes />
+-           <NotificationAlert />
+-         </div>
++         <App />               {/* ← Now you mount your full app */}
+      </UserProvider>
+    </NotificationProvider>
+  </BrowserRouter>
+</React.StrictMode>
 );
-
-export default SiteLayout;
