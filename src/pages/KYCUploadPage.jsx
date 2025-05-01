ec57e05be3306fb6874aebc0a9f38ff31e0dee6f
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-01 11:18:43 +0100

    Update KYCUploadPage.jsx

--- src/pages/KYCUploadPage.jsx
+++ src/pages/KYCUploadPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { toast } from 'react-hot-toast';
 import { getKYCStatus } from '@/api'; // assuming you’ve imported this already
+import { uploadKYC } from '@/api/optional'; // file-based KYC API
 import { useUser } from '@/context/UserContext';
 
 const KYCUploadPage = () => {
