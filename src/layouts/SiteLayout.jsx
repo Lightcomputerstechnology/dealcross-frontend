3596a09150303769987f3f5298d79b61eb8aadba
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-01 18:02:18 +0100

    Update SiteLayout.jsx

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,38 +1,25 @@
-// File: src/layouts/SiteLayout.jsx import React from 'react'; import { Outlet } from 'react-router-dom'; import SEOHead from '@/components/SEOHead'; import PromoBanner from '@/components/PromoBanner'; import Navbar from '@/components/Navbar'; import Footer from '@/components/Footer'; import BackToTopButton from '@/components/BackToTopButton'; import Logo from '@/assets/images/dealcross-logo.png';
+// File: src/layouts/SiteLayout.jsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import SEOHead from '@/components/SEOHead';
+import PromoBanner from '@/components/PromoBanner';
+import BackToTopButton from '@/components/BackToTopButton';
 
 const SiteLayout = () => (
+  <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900">
+    <SEOHead />
+    <PromoBanner />
+    <Navbar />
 
-  <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition duration-300 overflow-x-hidden">
-    {/* SEO Meta Tags */}
-    <SEOHead />{/* Watermark Background */}
-<div className="absolute inset-0 opacity-5 pointer-events-none z-0">
-  <img
-    src={Logo}
-    alt="Dealcross Watermark"
-    className="w-full h-full object-contain select-none"
-  />
-</div>
+    <main className="flex-grow p-4">
+      <Outlet />
+    </main>
 
-{/* Main Content Overlay */}
-<div className="relative z-10 flex flex-col flex-1">
-  {/* Top promotional banner */}
-  <PromoBanner />
-
-  {/* Navigation */}
-  <Navbar />
-
-  {/* Page content */}
-  <main className="flex-grow p-4 md:p-8">
-    <Outlet />
-  </main>
-
-  {/* Footer */}
-  <Footer />
-
-  {/* Back-to-top control */}
-  <BackToTopButton />
-</div>
-
+    <Footer />
+    <BackToTopButton />
   </div>
-);export default SiteLayout;
+);
 
+export default SiteLayout;
