0e520bea3dd33352cb738bb3fe0a759621825d20
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-02 04:05:27 +0100

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SiteLayout from '@/layouts/SiteLayout';
-import SignupPage from '@/pages/SignupPage'; // Or any page you’re sure works
+import SignupPage from '@/pages/SignupPage';
 
-const AppRoutes = () => (
-  <Routes>
-    <Route element={<SiteLayout />}>
-      <Route path="/" element={<SignupPage />} />
-    </Route>
-  </Routes>
-);
-
-export default AppRoutes;
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<SiteLayout />}>
+        <Route path="/" element={<SignupPage />} />
+      </Route>
+    </Routes>
+  );
+}
