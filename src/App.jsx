a1dd9f1823338a881ff1c6fe90463cc90f5a7296
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-02 03:39:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,23 @@ import React from 'react';
 import AppRoutes from './AppRoutes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
-
-// Global Layout Components
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-import FloatingChatButton from './components/FloatingChatButton.jsx';
-import NotificationAlert from './components/common/NotificationAlert.jsx';
-import NotificationPopUp from './components/common/NotificationPopUp.jsx';
-// Context Providers
 import { NotificationProvider } from './context/NotificationContext.jsx';
 
 function App() {
   return (
     <HelmetProvider>
       <NotificationProvider>
-        <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-all duration-300">
-          
-          {/* Top Navigation */}
-          <Navbar />
+        {/* Entire layout and routes are handled in SiteLayout.jsx */}
+        <AppRoutes />
 
-          {/* Main Application Routes */}
-          <div className="flex-grow">
-            <AppRoutes />
-          </div>
-
-          {/* Bottom Footer */}
-          <Footer />
-
-          {/* Global Overlays */}
-          <FloatingChatButton />
-          <NotificationAlert />
-          <NotificationPopUp />
-
-          {/* Toast Notifications */}
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              duration: 4000,
-              className: 'bg-white dark:bg-gray-800 text-black dark:text-white shadow-lg rounded',
-            }}
-          />
-        </div>
+        {/* Toast Notifications */}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            className: 'bg-white dark:bg-gray-800 text-black dark:text-white shadow-lg rounded',
+          }}
+        />
       </NotificationProvider>
     </HelmetProvider>
   );
