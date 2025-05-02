38e43c21a5f6163ff981d65d4b002654da00841a
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-02 04:04:52 +0100

    Update SiteLayout.jsx

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,3 +1,4 @@
+// File: src/layouts/SiteLayout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '@/components/Navbar';
@@ -5,40 +6,18 @@ import Footer from '@/components/Footer';
 import FloatingChatButton from '@/components/FloatingChatButton';
 import NotificationAlert from '@/components/common/NotificationAlert';
 import NotificationPopUp from '@/components/common/NotificationPopUp';
-import SEOHead from '@/components/SEOHead';
-import PromoBanner from '@/components/PromoBanner';
-import BackToTopButton from '@/components/BackToTopButton';
 
 const SiteLayout = () => {
   return (
-    <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition duration-300 overflow-x-hidden">
-      
-      {/* Global SEO */}
-      <SEOHead />
-
-      {/* Background Watermark (optional logo) */}
-      {/* <img
-        src="/favicon.png"
-        alt="Watermark"
-        className="absolute inset-0 w-full h-full object-contain opacity-5 pointer-events-none z-0 select-none"
-      /> */}
-
-      {/* Main Layout Content */}
-      <div className="relative z-10 flex flex-col min-h-screen">
-        <PromoBanner />
-        <Navbar />
-
-        {/* Page content */}
-        <main className="flex-grow">
-          <Outlet />
-        </main>
-
-        <Footer />
-        <BackToTopButton />
-        <FloatingChatButton />
-        <NotificationAlert />
-        <NotificationPopUp />
-      </div>
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-all duration-300">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+      <FloatingChatButton />
+      <NotificationAlert />
+      <NotificationPopUp />
     </div>
   );
 };
