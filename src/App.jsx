16a2883c4ae9ccdfbab8a2406ef0ea4f8a65e9e7
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-06 00:20:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 // File: src/App.jsx
 
-import React from 'react'; import AppRoutes from './AppRoutes.jsx'; import { HelmetProvider } from 'react-helmet-async'; import { Toaster } from 'react-hot-toast'; import { NotificationProvider } from './context/NotificationContext.jsx'; import FloatingChatButtons from './components/FloatingChatButtons.jsx';
+import React from 'react';
+import AppRoutes from './AppRoutes.jsx';
+import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
+import { NotificationProvider } from './context/NotificationContext.jsx';
+import FloatingChatButtons from './components/FloatingChatButtons.jsx';
 
-function App() { return ( <HelmetProvider> <NotificationProvider> <div className="relative min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-all duration-300"> <AppRoutes />
+// Optional enhancement: react-intersection-observer context provider
+import { IntersectionObserverProvider } from 'react-intersection-observer';
 
-{/* Floating Global Buttons */}
-      <FloatingChatButtons />
+function App() {
+  return (
+    <HelmetProvider>
+      <NotificationProvider>
+        <IntersectionObserverProvider>
+          <div className="relative min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-all duration-300">
+            <AppRoutes />
 
-      {/* Toasts */}
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          className: 'bg-white dark:bg-gray-800 text-black dark:text-white shadow-lg rounded',
-        }}
-      />
-    </div>
-  </NotificationProvider>
-</HelmetProvider>
+            {/* Floating Global Buttons */}
+            <FloatingChatButtons />
 
-); }
+            {/* Toasts */}
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration: 4000,
+                className: 'bg-white dark:bg-gray-800 text-black dark:text-white shadow-lg rounded',
+              }}
+            />
+          </div>
+        </IntersectionObserverProvider>
+      </NotificationProvider>
+    </HelmetProvider>
+  );
+}
 
 export default App;
-
